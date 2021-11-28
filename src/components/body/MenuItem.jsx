cf6f2adbf0ee0e2746c8680a245ae234b2adf334
easylearningbd <elebdsoft@gmail.com>
2021-11-29 03:30:32 +0600

    r27. Designing Menu Item

--- src/components/body/MenuItem.jsx
+++ src/components/body/MenuItem.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { Card } from "react-bootstrap";
  
 
 const MenuItem = props => {
-     console.log(props);
+     
      return (
           <div>
+
+<Card style={{ width: '25rem' }}>
+  <Card.Img variant="top" src={props.dish.image} />
+  <Card.Body>
+    <Card.Title style={{ cursor:"pointer" }} >{props.dish.name}</Card.Title>
+    <Card.Text>
+    {props.dish.description}
+    </Card.Text>
+    <p><b>Price : {props.dish.price}</b></p>
+    <p><b>Label : {props.dish.label}</b></p>
+    
+  </Card.Body>
+</Card>
                
           </div>
      )
