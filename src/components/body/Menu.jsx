e4f41480e0d8956f572d27deb8ed0a968746e3b7
easylearningbd <elebdsoft@gmail.com>
2021-11-29 03:48:32 +0600

    r28. DishDetail Component Part 1

--- src/components/body/Menu.jsx
+++ src/components/body/Menu.jsx
@@ -1,24 +1,33 @@
 import React, { Component } from 'react'
 import DISHES from '../../data/dishes.js'
+import DishDetail from './DishDetail.jsx'
 import MenuItem from './MenuItem.jsx'
 
 export class Menu extends Component {
 
      state = {
-          dishes:DISHES
+          dishes:DISHES,
+          selectedDish: null
      }
 
+     onDishSelect = dish => {
+          this.setState({selectedDish:dish});
+     }
 
 
      render() {
 
           const menu = this.state.dishes.map(item => {
                return (
-                    <MenuItem dish={item} key={item.id}  />
+                    <MenuItem dish={item} key={item.id} 
+                    DishSelect = {this.onDishSelect}  />
                )
           })
 
-
+          let dishDetail = null;
+          if(this.state.selectedDish != null){
+               dishDetail = <DishDetail dish={this.state.selectedDish} />
+          }
 
           return (
                <div className="container">
@@ -28,7 +37,7 @@ export class Menu extends Component {
                          </div>
 
                          <div className="col-6">
-                         <h1>Col 6 Right</h1>
+                        {dishDetail}
                            </div>
 
                     </div>
--- src/components/body/MenuItem.jsx
+++ src/components/body/MenuItem.jsx
@@ -10,7 +10,9 @@ const MenuItem = props => {
 <Card style={{ width: '25rem' }}>
   <Card.Img variant="top" src={props.dish.image} />
   <Card.Body>
-    <Card.Title style={{ cursor:"pointer" }} >{props.dish.name}</Card.Title>
+    <Card.Title style={{ cursor:"pointer" }} 
+    onClick={ () => props.DishSelect(props.dish)} >
+         {props.dish.name}</Card.Title>
     <Card.Text>
     {props.dish.description}
     </Card.Text>
