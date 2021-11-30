dad16ce0d812e3c851a910fcedd1896381508789
easylearningbd <elebdsoft@gmail.com>
2021-12-01 03:21:58 +0600

    r34 . Adding Routes

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Menu from "./Menu";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
 
 const Body = () => {
      return (
           <div>
-              <Menu/>
+                <Routes>
+     <Route path="/" element={<Home/>} />
+     <Route path="/menu" element={<Menu/>} />
+     <Route path="/about" element={<About/>} />
+     <Route path="/contact" element={<Contact/>} />
+              
+              </Routes>
           </div>
      )
 
--- src/components/header/Navigation.jsx
+++ src/components/header/Navigation.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import {Container,Navbar,Nav} from 'react-bootstrap'
+import { Link } from "react-router-dom";
+import '../../assets/css/Main.css';
+import Logo from '../../assets/image/logo.png'
 
 const Navigation = () => {
      return (
           <div>
-               <Navbar bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand href="#home">Easy Restaurant</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>
+               <Navbar bg="dark" expand="lg">
+  <Container>
+    <Navbar.Brand > <Link to="/"> <img src={Logo} />  </Link> </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link> <Link to="/" className="mainmenu" > Home </Link> </Nav.Link>
+        <Nav.Link> <Link to="/menu"  className="mainmenu"> Menu </Link> </Nav.Link>
+        <Nav.Link> <Link to="/about" className="mainmenu"> About </Link> </Nav.Link>
+        <Nav.Link> <Link to="/contact" className="mainmenu"> Contact </Link> </Nav.Link>
+         
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
           </div>
      )
 }
